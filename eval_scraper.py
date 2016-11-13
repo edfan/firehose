8d95f936c034c5a7202e68a889a39014350af1b9
Edward Fan <edwardzfan@gmail.com>
2016-11-13 15:22:08 -0500

    Add professors to scraper; added precomputed average to scraper; removed data pending rescrape

--- eval_scraper.py
+++ eval_scraper.py
@@ -1,21 +1,23 @@
 #! /usr/bin/env python3
 
 import selenium
-import time
-import pickle
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
+
+import time
+import pickle
+from decimal import *
 
 username = 'edwardf'
 with open('password', 'r') as f:
     password = f.read().strip()
 
 eval_url = 'https://edu-apps.mit.edu/ose-rpt/subjectEvaluationSearch.htm'
-courses = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 17, 18, 20, 22, 24, 'AS', 'EC', 'MS',
-           'NS', '21A', '21G', '21H', '21L', '21M', '21W', 'CMS', 'CON', 'CSB', 'ESD', 'ESG', 'HST',
-           'ISP', 'MAS', 'STS', 'WGS']
+courses = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 17, 18, 20, 22, 24, 'AS', 'EC',
+           'MS', 'NS', '21A', '21G', '21H', '21L', '21M', '21W', 'CMS', 'CON', 'CSB', 'ESD',
+           'ESG', 'HST', 'ISP', 'MAS', 'STS', 'WGS']
 
 terms = ['2015FA', '2015JA', '2015SP', '2015SU',
          '2016FA', '2016JA', '2016SP', '2016SU']
@@ -31,53 +33,78 @@ def mit_duo_login():
     session.get(eval_url)
 
     # Fill in Kerberos login form
-    username_field = session.find_element_by_name('j_username')
-    password_field = session.find_element_by_name('j_password')
-    username_field.send_keys(username)
-    password_field.send_keys(password)
+    session.find_element_by_name('j_username').send_keys(username)
+    session.find_element_by_name('j_password').send_keys(password)
 
     session.find_element_by_name('Submit').click()
 
     # Send push Duo authentication
     WebDriverWait(session, 10).until(EC.frame_to_be_available_and_switch_to_it('duo_iframe'))
-    duo_button = session.find_element_by_xpath('//*[contains(text(), "Send Me a Push")]')
-    duo_button.click()
+    session.find_element_by_xpath('//*[contains(text(), "Send Me a Push")]').click()
 
     # Wait until base page loads
     WebDriverWait(session, 60).until(EC.title_is('Subject Evaluation Report Search'))
 
     return session
 
-def scrape_class_info(session, class_element, class_dict):
+def scrape_class_info(session, class_element, class_dict, term):
     # Click link and wait until report loads
     url = class_element.get_attribute('href')
     class_element.click()
     WebDriverWait(session, 60).until(EC.title_contains('Report for'))
 
-    title_element = session.find_element_by_xpath("/html/body/div[@id='contentsframe']/table[@class='header']/tbody/tr[1]/td[@class='subjectTitle']/h1")
-    titles = title_element.text.split('\n')
-
     # Classes can have different numbers & different names
+    titles = session.find_element_by_xpath("/html/body/div[@id='contentsframe']/table[@class='header']/tbody/tr[1]/td[@class='subjectTitle']/h1").text.split('\n')
+
     class_numbers = [t.split(' ')[0] for t in titles]
     class_names = [' '.join(t.split(' ')[1:]) for t in titles]
 
-    rating_element = session.find_element_by_xpath("/html/body/div[@id='contentsframe']/table[@class='header']/tbody/tr[2]/td[@class='summaryContainer']/table[@class='summary']/tbody/tr/td[4]/p")
-    rating = rating_element.text[27:30]
+    # Add static ratings/stats to dict
+    cd = {}
+    cd['term'] = term
+    cd['url'] = url
+    
+    cd['rating'] = Decimal(session.find_element_by_xpath("/html/body/div[@id='contentsframe']/table[@class='header']/tbody/tr[2]/td[@class='summaryContainer']/table[@class='summary']/tbody/tr/td[4]/p").text[27:30])
 
-    ic_hours_element = session.find_element_by_xpath("/html/body/div[@id='contentsframe']/table[@class='indivQuestions'][3]/tbody/tr[4]/td[@class='avg']")
-    ic_hours = ic_hours_element.text
+    cd['ic_hours'] = Decimal(session.find_element_by_xpath("/html/body/div[@id='contentsframe']/table[@class='indivQuestions'][3]/tbody/tr[4]/td[@class='avg']").text)
 
-    oc_hours_element = session.find_element_by_xpath("/html/body/div[@id='contentsframe']/table[@class='indivQuestions'][3]/tbody/tr[5]/td[@class='avg']")
-    oc_hours = oc_hours_element.text
+    cd['oc_hours'] = Decimal(session.find_element_by_xpath("/html/body/div[@id='contentsframe']/table[@class='indivQuestions'][3]/tbody/tr[5]/td[@class='avg']").text)
+
+    cd['eligible'] = int(session.find_element_by_xpath("/html/body/div[@id='contentsframe']/table[@class='header']/tbody/tr[2]/td[@class='summaryContainer']/table[@class='summary']/tbody/tr/td[1]").text.split()[3])
+
+    cd['resp'] = int(session.find_element_by_xpath("/html/body/div[@id='contentsframe']/table[@class='header']/tbody/tr[2]/td[@class='summaryContainer']/table[@class='summary']/tbody/tr/td[2]").text.split()[4])
+
+    cd['rate'] = int(session.find_element_by_xpath("/html/body/div[@id='contentsframe']/table[@class='header']/tbody/tr[2]/td[@class='summaryContainer']/table[@class='summary']/tbody/tr/td[3]").text.split()[2].rstrip('%'))
+
+    # Add professors to dict
+    cd['professors'] = []
+
+    try:
+        for x in range(3, 100):
+            prof = {}
+
+            prof['name'] = session.find_element_by_xpath("/html/body/div[@id='contentsframe']/table[@class='grid']/tbody/tr[{}]/td[1]/a/strong".format(x)).text
+
+            prof['rating'] = Decimal(session.find_element_by_xpath("/html/body/div[@id='contentsframe']/table[@class='grid']/tbody/tr[{}]/td[5]/span[@class='avg']".format(x)).text)
+
+            prof['role'] = session.find_element_by_xpath("/html/body/div[@id='contentsframe']/table[@class='grid']/tbody/tr[{}]/td[1]".format(x)).text.replace(prof['name'], '').rstrip('(RLECAB)').strip(' ,')
+
+            cd['professors'].append(prof)
+                
+    except NoSuchElementException:
+        pass        
+    
 
     # Fits perfectly in a cmd window
-    print('{:8} | {:48.48} | {} | {} | {}'.format(class_numbers[0], class_names[0], rating, ic_hours, oc_hours))
+    print('{:8} | {:48.48} | {} | {} | {}'.format(class_numbers[0], class_names[0], cd['rating'], cd['ic_hours'], cd['oc_hours']))
 
     for i in range(len(titles)):
         class_num_split = class_numbers[i].split('.')
-        class_dict[class_numbers[i]] = (class_num_split[0], class_num_split[1],
-                                        class_names[i], rating, ic_hours,
-                                        oc_hours, url)
+        cd['course_number'] = class_num_split[0]
+        cd['class_number'] = class_num_split[1]
+        cd['class_name'] = class_names[i]
+        
+        class_dict[class_numbers[i]] = cd
         
     # Error-resistant back, then wait for search page
     session.execute_script("window.history.go(-1)")
@@ -96,7 +123,7 @@ def main():
         for i in range(4, 2000):
             try:
                 class_element = session.find_element_by_xpath("/html/body/div[@id='wrapper']/div[@id='rh-col']/p[{}]/a".format(i))
-                scrape_class_info(session, class_element, class_dict)
+                scrape_class_info(session, class_element, class_dict, term)
             except NoSuchElementException:
                 continue
 
--- eval_to_json.py
+++ eval_to_json.py
@@ -4,6 +4,13 @@ import json
 import pickle
 import os
 import re
+from decimal import *
+
+class DecimalEncoder(json.JSONEncoder):
+    def _iterencode(self, o, markers=None):
+        if isinstance(o, decimal.Decimal):
+            return (str(o) for o in [o])
+        return super(DecimalEncoder, self)._iterencode(o, markers)
 
 def atoi(text):
     return int(text) if text.isdigit() else text
@@ -14,30 +21,44 @@ def natural_keys(d):
 
 def main():
     classes = {}
-    
+
+    # Pull all terms into a single list for each class
     for term in os.listdir('data/'):
         with open('data/' + term, 'rb') as f:
             term_dict = pickle.load(f)
         for k, v in term_dict.items():
             if k not in classes:
                 classes[k] = []
-            ct_dict = {
-                'term': term,
-                'course_number': v[0],
-                'class_number': v[1],
-                'class_name': v[2],
-                'rating': v[3],
-                'ic_hours': v[4],
-                'oc_hours': v[5]}
-            classes[k].append(ct_dict)
+            classes[k].append(v)
 
+    # Compute an average and stick it in index 0
+    for k, v in classes.items():
+        term_count = len(v)
+        average = { 'term': 'average',
+                    'rating': 0,
+                    'ic_hours': 0,
+                    'oc_hours': 0,
+                    'class_name': v[-1]['class_name'] }
+
+        for term in v:
+            average['rating'] += term['rating']
+            average['ic_hours'] += term['ic_hours']
+            average['oc_hours'] += term['oc_hours']
+
+        average['rating'] /= term_count
+        average['ic_hours'] /= term_count
+        average['oc_hours'] /= term_count
+
+        classes['k'] = [average] + v
+            
     classes = sorted([{k: v} for k, v in classes.items()],
                      key=natural_keys)
             
     with open('www/evaluations.json', 'w') as f:
         f.write('var evals = ')
-        json.dump(classes, f)
+        json.dump(classes, f, cls=DecimalEncoder)
         f.write(';')
 
 if __name__ == '__main__':
+    getcontext().prec = 1
     main()
