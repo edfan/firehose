a7afe737dfaf0a079c0953edb79df892862bb83d
Edward Fan <edwardf@mit.edu>
2017-12-05 15:54:49 -0500

    IAP cut 1

--- new_scripts/combiner.py
+++ new_scripts/combiner.py
@@ -97,35 +97,37 @@ for c in times:
         classes[c]['h'] = 0
         classes[c]['si'] = 0
 
+try:
+    # Special case 14.01/14.02 rec-only sections.
+    classes['14.01R'] = copy.deepcopy(classes['14.01'])
+    classes['14.01']['r'] = classes['14.01']['r'][:3] + [classes['14.01']['r'][5]]
+    classes['14.01']['rr'] = classes['14.01']['rr'][:3] + [classes['14.01']['rr'][5]]
+    classes['14.01R']['no'] = '14.01R'
+    classes['14.01R']['s'] = ['r']
+    classes['14.01R']['r'] = classes['14.01R']['r'][3:5]
+    classes['14.01R']['rr'] = classes['14.01R']['rr'][3:5]
+    classes['14.01R']['n'] += " (recitation only)"
+    del classes['14.01R']['l']
 
-# Special case 14.01/14.02 rec-only sections.
-classes['14.01R'] = copy.deepcopy(classes['14.01'])
-classes['14.01']['r'] = classes['14.01']['r'][:3] + [classes['14.01']['r'][5]]
-classes['14.01']['rr'] = classes['14.01']['rr'][:3] + [classes['14.01']['rr'][5]]
-classes['14.01R']['no'] = '14.01R'
-classes['14.01R']['s'] = ['r']
-classes['14.01R']['r'] = classes['14.01R']['r'][3:5]
-classes['14.01R']['rr'] = classes['14.01R']['rr'][3:5]
-classes['14.01R']['n'] += " (recitation only)"
-del classes['14.01R']['l']
+    # Special case 16.001/16.003 evals.
+    #classes['16.001']['h'] = 11.6
 
-# Special case 16.001/16.003 evals.
-#classes['16.001']['h'] = 11.6
+    # Special case 18.01A description.
+    #classes['18.01A']['d'] = "Six-week review of one-variable calculus, emphasizing material not on the high-school AB syllabus: integration techniques and applications, improper integrals, infinite series, applications to other topics, such as probability and statistics, as time permits. Prerequisites: one year of high-school calculus or the equivalent, with a score of 5 on the AB Calculus test (or the AB portion of the BC test, or an equivalent score on a standard international exam), or equivalent college transfer credit, or a passing grade on the first half of the 18.01 advanced standing exam. "
 
-# Special case 18.01A description.
-#classes['18.01A']['d'] = "Six-week review of one-variable calculus, emphasizing material not on the high-school AB syllabus: integration techniques and applications, improper integrals, infinite series, applications to other topics, such as probability and statistics, as time permits. Prerequisites: one year of high-school calculus or the equivalent, with a score of 5 on the AB Calculus test (or the AB portion of the BC test, or an equivalent score on a standard international exam), or equivalent college transfer credit, or a passing grade on the first half of the 18.01 advanced standing exam. "
+    # Special case 2.S991 description.
+    classes['2.S991']['n'] = "Designing the First Year at MIT"
+    classes['2.S991']['d'] = "This subject will offer instruction in the process of design while working on a specific design challenge: the potential to significantly improve and innovate on the MIT undergraduate first year. Using design methods from across MIT Schools, students will learn about the design process beginning with identifying needs and goals to developing concepts and modes of validation. Beginning with stakeholder needs identification that will involve reaching out directly to the MIT community, students will be responsible for project deliverables including a customer needs document and a tradespace of options. Subject work will be team-based and project-focused, offering students an opportunity to present the audacious and incremental options developed to senior MIT stakeholders in a workshop format. Students will also be exposed to principles of curriculum design and pedagogy, as they develop a holistic perspective to the design of the MIT First Year."
+    classes['2.S991']['u1'] = 3
+    classes['2.S991']['u3'] = 9
+    classes['2.S991']['he'] = True
 
-# Special case 2.S991 description.
-classes['2.S991']['n'] = "Designing the First Year at MIT"
-classes['2.S991']['d'] = "This subject will offer instruction in the process of design while working on a specific design challenge: the potential to significantly improve and innovate on the MIT undergraduate first year. Using design methods from across MIT Schools, students will learn about the design process beginning with identifying needs and goals to developing concepts and modes of validation. Beginning with stakeholder needs identification that will involve reaching out directly to the MIT community, students will be responsible for project deliverables including a customer needs document and a tradespace of options. Subject work will be team-based and project-focused, offering students an opportunity to present the audacious and incremental options developed to senior MIT stakeholders in a workshop format. Students will also be exposed to principles of curriculum design and pedagogy, as they develop a holistic perspective to the design of the MIT First Year."
-classes['2.S991']['u1'] = 3
-classes['2.S991']['u3'] = 9
-classes['2.S991']['he'] = True
-
-# Special case 6.08 / 6.S08.
-classes['6.08']['ra'] = 6.6
-classes['6.08']['h'] = 12.3
-classes['6.08']['si'] = 109.5
+    # Special case 6.08 / 6.S08.
+    classes['6.08']['ra'] = 6.6
+    classes['6.08']['h'] = 12.3
+    classes['6.08']['si'] = 109.5
+except:
+    pass
 
 with open('full.json', 'w') as f:
     f.write('var classes = ')
