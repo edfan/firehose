2ecd21ff0dd808ca115f56dbcbeb3d8d8e154e34
Edward Fan <edwardf@mit.edu>
2020-08-31 22:39:48 -0400

    Update calendar name

--- www/script.js
+++ www/script.js
@@ -1022,7 +1022,7 @@ function calendar_send(isSignedIn) {
 		$("#calendar-link").text("Working...");
 
 		gapi.client.calendar.calendarList.list({}).then(function (resp) {
-			var name = "Firehose: Spring 2020";
+			var name = "Firehose: Fall 2020";
 			var ids = [];
 
 			for (var i in resp.result.items) {
