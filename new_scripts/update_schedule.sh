6b3f31d2b95bcba40a3494936827a7a38b5c068a
CJ Quines <cj@cjquines.com>
2021-08-19 12:27:15 -0400

    add update schedule script

---
+++ new_scripts/update_schedule.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+cd "${0%/*}"
+echo "=== coursews.py ==="
+python3 coursews.py
+echo "=== sublist_ws.py ==="
+python3 sublist_ws.py
+echo "=== combiner_ws.py ==="
+python3 combiner_ws.py
+cp full.json ../www/full.js
