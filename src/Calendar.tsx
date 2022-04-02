2d7433559d7d5ebf75463b57d1b1ce163373eae2
CJ Quines <cj@cjquines.com>
2022-04-02 19:09:55 -0400

    initial calendar setup

--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -26,7 +26,23 @@ export function Calendar(props: {
         {/* <div id="warning3-div" style="display: none;">Too many options? Use the "+ Manually set sections" button above the class description to lock recitation times.</div> */}
       </div>
       <div id="left-int-div">
-        <div id="calendar"></div>
+        <div id="calendar">
+          <FullCalendar
+            plugins={[timeGridPlugin]}
+            initialView="timeGridWeek"
+            allDaySlot={false}
+            dayHeaderFormat={{ weekday: "long" }}
+            editable={false}
+            events={currentActivities.flatMap((act) => act.events)}
+            headerToolbar={false}
+            height="auto"
+            // a date that is, conveniently enough, a monday
+            initialDate="2001-01-01"
+            slotMinTime="08:00:00"
+            slotMaxTime="22:00:00"
+            weekends={false}
+          />
+        </div>
       </div>
     </>
   );
